a19902e07a90f23026de3777c583dbfb1711daa4
Matthew Canham <mdcanham@me.com>
2015-02-19 11:11:46 +1300

    Updated libraries.

--- libraries/LiquidCrystal_I2C/LiquidCrystal_I2C.cpp
+++ libraries/LiquidCrystal_I2C/LiquidCrystal_I2C.cpp
@@ -1,9 +1,7 @@
 #include "LiquidCrystal_I2C.h"
 #include <inttypes.h>
-#include "WProgram.h" 
-#include "Wire.h"
-
-
+#include <Arduino.h>
+#include <Wire.h>
 
 // When the display powers up, it is configured as follows:
 //
@@ -24,64 +22,55 @@
 // can't assume that its in that state when a sketch starts (and the
 // LiquidCrystal constructor is called).
 
-LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows)
+LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize)
 {
-  _Addr = lcd_Addr;
-  _cols = lcd_cols;
-  _rows = lcd_rows;
-  _backlightval = LCD_NOBACKLIGHT;
+	_addr = lcd_addr;
+	_cols = lcd_cols;
+	_rows = lcd_rows;
+	_charsize = charsize;
+	_backlightval = LCD_BACKLIGHT;
 }
 
-void LiquidCrystal_I2C::init(){
-	init_priv();
-}
-
-void LiquidCrystal_I2C::init_priv()
-{
+void LiquidCrystal_I2C::begin() {
 	Wire.begin();
 	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-	begin(_cols, _rows);  
-}
 
-void LiquidCrystal_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
-	if (lines > 1) {
+	if (_rows > 1) {
 		_displayfunction |= LCD_2LINE;
 	}
-	_numlines = lines;
 
 	// for some 1 line displays you can select a 10 pixel high font
-	if ((dotsize != 0) && (lines == 1)) {
+	if ((_charsize != 0) && (_rows == 1)) {
 		_displayfunction |= LCD_5x10DOTS;
 	}
 
 	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
 	// according to datasheet, we need at least 40ms after power rises above 2.7V
 	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
-	delayMicroseconds(50000); 
-  
+	delay(50); 
+
 	// Now we pull both RS and R/W low to begin commands
 	expanderWrite(_backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
 	delay(1000);
 
-  	//put the LCD into 4 bit mode
+	//put the LCD into 4 bit mode
 	// this is according to the hitachi HD44780 datasheet
 	// figure 24, pg 46
-	
-	// we start in 8bit mode, try to set 4 bit mode
-	write4bits(0x03);
-	delayMicroseconds(4500); // wait min 4.1ms
-	
-	// second try
-	write4bits(0x03);
-	delayMicroseconds(4500); // wait min 4.1ms
-	
-	// third go!
-	write4bits(0x03); 
-	delayMicroseconds(150);
-	
-	// finally, set to 4-bit interface
-	write4bits(0x02); 
 
+	// we start in 8bit mode, try to set 4 bit mode
+	write4bits(0x03 << 4);
+	delayMicroseconds(4500); // wait min 4.1ms
+
+	// second try
+	write4bits(0x03 << 4);
+	delayMicroseconds(4500); // wait min 4.1ms
+
+	// third go!
+	write4bits(0x03 << 4); 
+	delayMicroseconds(150);
+
+	// finally, set to 4-bit interface
+	write4bits(0x02 << 4); 
 
 	// set # lines, font size, etc.
 	command(LCD_FUNCTIONSET | _displayfunction);  
@@ -100,10 +89,7 @@ void LiquidCrystal_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
 	command(LCD_ENTRYMODESET | _displaymode);
 	
 	home();
-  
 }
-
-
 
 /********** high level commands, for the user! */
 void LiquidCrystal_I2C::clear(){
@@ -118,8 +104,8 @@ void LiquidCrystal_I2C::home(){
 
 void LiquidCrystal_I2C::setCursor(uint8_t col, uint8_t row){
 	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-	if ( row > _numlines ) {
-		row = _numlines-1;    // we count rows starting w/0
+	if (row > _rows) {
+		row = _rows-1;    // we count rows starting w/0
 	}
 	command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
 }
@@ -207,28 +193,25 @@ void LiquidCrystal_I2C::backlight(void) {
 	expanderWrite(0);
 }
 
-
-
 /*********** mid level commands, for sending data/cmds */
 
 inline void LiquidCrystal_I2C::command(uint8_t value) {
 	send(value, 0);
 }
 
-inline void LiquidCrystal_I2C::write(uint8_t value) {
+inline size_t LiquidCrystal_I2C::write(uint8_t value) {
 	send(value, Rs);
 }
-
 
 
 /************ low level data pushing commands **********/
 
 // write either command or data
 void LiquidCrystal_I2C::send(uint8_t value, uint8_t mode) {
-	uint8_t highnib=value>>4;
-	uint8_t lownib=value & 0x0F;
+	uint8_t highnib=value&0xf0;
+	uint8_t lownib=(value<<4)&0xf0;
 	write4bits((highnib)|mode);
-	write4bits((lownib)|mode);
+	write4bits((lownib)|mode); 
 }
 
 void LiquidCrystal_I2C::write4bits(uint8_t value) {
@@ -237,8 +220,8 @@ void LiquidCrystal_I2C::write4bits(uint8_t value) {
 }
 
 void LiquidCrystal_I2C::expanderWrite(uint8_t _data){                                        
-	Wire.beginTransmission(_Addr);
-	Wire.send((int)(_data) | _backlightval);
+	Wire.beginTransmission(_addr);
+	Wire.write((int)(_data) | _backlightval);
 	Wire.endTransmission();   
 }
 
@@ -248,35 +231,16 @@ void LiquidCrystal_I2C::pulseEnable(uint8_t _data){
 	
 	expanderWrite(_data & ~En);	// En low
 	delayMicroseconds(50);		// commands need > 37us to settle
-} 
-
-
-// Alias functions
-
-void LiquidCrystal_I2C::cursor_on(){
-	cursor();
-}
-
-void LiquidCrystal_I2C::cursor_off(){
-	noCursor();
-}
-
-void LiquidCrystal_I2C::blink_on(){
-	blink();
-}
-
-void LiquidCrystal_I2C::blink_off(){
-	noBlink();
 }
 
 void LiquidCrystal_I2C::load_custom_character(uint8_t char_num, uint8_t *rows){
-		createChar(char_num, rows);
+	createChar(char_num, rows);
 }
 
 void LiquidCrystal_I2C::setBacklight(uint8_t new_val){
-	if(new_val){
+	if (new_val) {
 		backlight();		// turn backlight on
-	}else{
+	} else {
 		noBacklight();		// turn backlight off
 	}
 }
@@ -286,17 +250,3 @@ void LiquidCrystal_I2C::printstr(const char c[]){
 	//it's here so the user sketch doesn't have to be changed 
 	print(c);
 }
-
-
-// unsupported API functions
-void LiquidCrystal_I2C::off(){}
-void LiquidCrystal_I2C::on(){}
-void LiquidCrystal_I2C::setDelay (int cmdDelay,int charDelay) {}
-uint8_t LiquidCrystal_I2C::status(){return 0;}
-uint8_t LiquidCrystal_I2C::keypad (){return 0;}
-uint8_t LiquidCrystal_I2C::init_bargraph(uint8_t graphtype){return 0;}
-void LiquidCrystal_I2C::draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end){}
-void LiquidCrystal_I2C::draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_row_end){}
-void LiquidCrystal_I2C::setContrast(uint8_t new_val){}
-
-	
--- src/src.ino
+++ src/src.ino
@@ -1,8 +1,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup(){
-  
+  dht.begin();
 }
 
 void loop(){
